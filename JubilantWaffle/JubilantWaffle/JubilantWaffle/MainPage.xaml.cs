1a290d5837946c888688e863bfbb3b46cc2cbe16
Maximilien Louis Dal Viallon <maximilien.viallon@gmail.com>
2020-02-23 22:49:07 +0100

    added Dynamic button creation

--- JubilantWaffle/JubilantWaffle/JubilantWaffle/MainPage.xaml.cs
+++ JubilantWaffle/JubilantWaffle/JubilantWaffle/MainPage.xaml.cs
@@ -16,6 +16,7 @@ namespace JubilantWaffle
         public MainPage()
         {
             InitializeComponent();
+            
         }
     }
 }
--- JubilantWaffle/JubilantWaffle/JubilantWaffle/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ JubilantWaffle/JubilantWaffle/JubilantWaffle/obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -17,8 +17,16 @@ namespace JubilantWaffle {
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry puzzleSize;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Button puzzleSizeButton;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
+            puzzleSize = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "puzzleSize");
+            puzzleSizeButton = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Button>(this, "puzzleSizeButton");
         }
     }
 }
