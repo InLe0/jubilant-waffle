dfe7e67b12f7ad782f0b354d8c952fe330f0161e
InLe0 <rockeater0@gmail.com>
2020-02-23 21:26:06 +0000

    commitment

--- JubilantWaffle/JubilantWaffle/JubilantWaffle.Android/obj/Debug/90/android/environment.x86.s
+++ JubilantWaffle/JubilantWaffle/JubilantWaffle.Android/obj/Debug/90/android/environment.x86.s
@@ -61,7 +61,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"95bc2297-3076-4f4c-a38b-467c692c0701"
+	.asciz	"2179b1df-29f9-4f15-9162-aeebd1096d32"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
