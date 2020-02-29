ae873b6aaef03c00f0bf7d628b1269caa5d74dd8
InLe0 <rockeater0@gmail.com>
2020-02-29 21:35:50 +0000

    commit commit commit

--- JubilantWaffle/JubilantWaffle/JubilantWaffle.Android/obj/Debug/90/android/environment.armeabi-v7a.s
+++ JubilantWaffle/JubilantWaffle/JubilantWaffle.Android/obj/Debug/90/android/environment.armeabi-v7a.s
@@ -82,7 +82,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",%progbits,1
 	.type	.L.str.8, %object
 .L.str.8:
-	.asciz	"02d969fb-a453-4615-b3fe-483f2327263b"
+	.asciz	"acfb6915-34d2-4d5a-b6f1-db347539dd48"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",%progbits,1
 	.type	.L.str.9, %object
