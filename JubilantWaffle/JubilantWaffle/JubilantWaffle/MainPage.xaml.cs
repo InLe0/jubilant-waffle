f4cd5440abd4032cb2ad5ae0813a184ad347afe9
InLe0 <rockeater0@gmail.com>
2020-03-02 18:09:04 +0000

    Common courtesy

--- JubilantWaffle/JubilantWaffle/JubilantWaffle/MainPage.xaml.cs
+++ JubilantWaffle/JubilantWaffle/JubilantWaffle/MainPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using Xamarin.Forms;
 
-
+// Icon obtained from @IconkaCom
 namespace JubilantWaffle
 {
     [DesignTimeVisible(false)]
