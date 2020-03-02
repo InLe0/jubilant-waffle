91c378a73496945b25630c19be44368dcd548e42
InLe0 <rockeater0@gmail.com>
2020-03-02 17:56:16 +0000

    Do not pull!!

--- JubilantWaffle/JubilantWaffle/JubilantWaffle.Android/obj/Debug/90/android/environment.x86.s
+++ JubilantWaffle/JubilantWaffle/JubilantWaffle.Android/obj/Debug/90/android/environment.x86.s
@@ -61,7 +61,7 @@ mono_aot_mode_name:
 	.section	.rodata..L.str.8,"aMS",@progbits,1
 	.type	.L.str.8, @object
 .L.str.8:
-	.asciz	"f5e2fa49-c849-4ea6-a83e-29bea8c3e8a5"
+	.asciz	"df8795d0-f4a2-4a2c-90c7-5bac8edb9cef"
 	.size	.L.str.8, 37
 	.section	.rodata..L.str.9,"aMS",@progbits,1
 	.type	.L.str.9, @object
